da661009717d2d0be90f784896a91a98ab3f588d
Jeffrey419 <96989442+Jeffrey419@users.noreply.github.com>
2022-01-17 21:20:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 var title = "website"
 document.getElementById("title").innerHTML = title;
+
+
+window.onscroll = function() {makeItStick()};
+
+var navbar = document.getElementById("top");
+var sticky = navbar.offsetTop;
+
+function makeItStick(){
+	if (window.pageYOffset >= sticky) {
+		navbar.style.position = "fixed";
+		navbar.style.top = 0;
+	} else {
+		navbar.style.position = 'absolute';
+		navbar.style.top = 0;
+	}
+}
