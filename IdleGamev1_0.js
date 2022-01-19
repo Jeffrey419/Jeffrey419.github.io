1a7e69993c8bdeb91eb7221734316b8b7add319f
Jeffrey419 <96989442+Jeffrey419@users.noreply.github.com>
2022-01-19 09:18:34 -0600

    Update IdleGamev1_0.js

--- IdleGamev1_0.js
+++ IdleGamev1_0.js
@@ -1,11 +1,14 @@
 //local storage stuff
 if (typeof(storage) != undefined) {
 if (!localStorage.moneys) {localStorage.moneys = 10;}
+if (!localStorage.AmountOfNormalBalls) {localStorage.AmountOfNormalBalls = 0;}
 } else {alert('your browser does not support webstorage');}
 
 
 //variables
 var money = localStorage.moneys;
+var Normalballs = [];
+var AmountOfNormalBalls = localStorage.AmountOfNormalBalls;
 
 
 //store, place to buy stuff
